a65ce4b4666664951ca6085a45ef19f2aa9f7c61
udyanojha <use 51847297+udyanojha@users.noreply.github.com>
2022-08-31 01:50:11 +0530

    added smooth scroll css

---
+++ main.js
@@ -0,0 +1,11 @@
+// const doc = document;
+
+// const nav_about = doc.getElementById("nav-about");
+// const nav_skills = doc.getElementById("nav-skills");
+// const nav_exp = doc.getElementById("nav-exp");
+// const nav_edu = doc.getElementById("nav-edu");
+// const nav_portforlio = doc.getElementById("nav-portfolio");
+
+// function scroll() {
+
+// }
